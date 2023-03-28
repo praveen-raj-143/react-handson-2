d1285a103a4e76c256bdb6b7085c190d88997eba
praveen-raj-143 <praveenrajgreath@gmail.com>
2023-03-28 21:28:07 +0530

    modified installed nodemon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('<h1>EXPRESS ?</h1><br/><h2>Express is a popular Node.js framework for building web applcations.It provides a set of tools and utilities for creating server-side applications that can handle HTTP requests and responses, manage routing, and interact with databases and other third-party services.</h2>')
+  res.send('<h1>EXPRESS </h1><br/><h2>Express is a popular Node.js framework for building web applcations.It provides a set of tools and utilities for creating server-side applications that can handle HTTP requests and responses, manage routing, and interact with databases and other third-party services.</h2>')
   
 })
 
